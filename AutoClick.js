1ac7a0ebb8bb89f4dbc473d4c89772d3770f5982
faroukbmiled <fcfaroukfc@gmail.com>
2022-12-30 19:09:25 +0100

    random scripts

---
+++ AutoClick.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name        Click on most recent link altenens
+// @namespace   None
+// @match *://altenens.is/forums/*
+// @version     2.0
+// @author      Ryuk
+// @grant       window.focus
+// @description Opens the link with the "A moment ago" time element in a new tab and focuses on the window
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Get the time element with the text "A moment ago"
+    let targetTimeElement = null;
+    const timeElements = document.querySelectorAll('time.u-dt:not([class*=" "])');
+    timeElements.forEach(timeElement => {
+        if (timeElement.textContent === 'A moment ago') {
+            targetTimeElement = timeElement;
+            return;
+        }
+    });
+
+    if (targetTimeElement) {
+        const dataTimeValue = targetTimeElement.getAttribute('data-time');
+        if (dataTimeValue === localStorage.getItem('lastCheckedDataTimeValue')) {
+            console.log('The data-time value has not changed, so the link will not be clicked.');
+        } else {
+            console.log('The data-time value has changed, so the link will be clicked.');
+            localStorage.setItem('lastCheckedDataTimeValue', dataTimeValue);
+            const linkElement = targetTimeElement.closest('a');
+            if (linkElement) {
+                window.open(linkElement.href, '_blank');
+            }
+        }
+    }
+})();
---
+++ AutoCopy.js
@@ -0,0 +1,98 @@
+// ==UserScript==
+// @name        Auto copy example
+// @namespace   none
+// @match *://altenens.is/threads*
+// @grant       none
+// @version     1.5
+// @author      Ryuk
+// @description Copies the first 3/5 and 16-digit numbers found on the page to the clipboard
+// @grant clipboardWrite
+// @require https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const elements = document.querySelectorAll('*');
+
+    let found3DigitNumber = false;
+    let found16DigitNumber = false;
+    let found5DigitNumber = false;
+    let matches16 = null;
+    let matches3 = null;
+    let matches5 = null;
+
+    for (let element of elements) {
+        if (!found16DigitNumber) {
+            const regex16 = /\d{16}/g;
+            matches16 = element.textContent.match(regex16);
+            if (matches16) {
+                console.log(matches16);
+                found16DigitNumber = true;
+            }
+        }
+
+        if (found16DigitNumber) {
+            const regex3 = /(?<!\d)\d{3}(?!\d)/g;
+            matches3 = element.textContent.substring(element.textContent.indexOf(matches16[0]) + matches16[0].length).match(regex3);
+            if (matches3) {
+                found3DigitNumber = true;
+            }
+        }
+
+        if (found3DigitNumber) {
+            const regex5 = /(?<!\d)\d{5}(?!\d)/g;
+            matches5 = element.textContent.match(regex5);
+            console.log(matches5);
+            found5DigitNumber = true;
+        }
+
+        if (found3DigitNumber && found16DigitNumber) {
+            break;
+        }
+    }
+
+    if (!matches5 || !found5DigitNumber) {
+        const random5DigitNumber = 27808;
+
+        navigator.clipboard.writeText(random5DigitNumber).then(() => {
+            console.log(`Const 5 ${random5DigitNumber} written to clipboard`);
+
+            setTimeout(() => {
+                navigator.clipboard.writeText(matches3[0]).then(() => {
+                    console.log(`3 ${matches3[0]} written to clipboard`);
+
+                    setTimeout(() => {
+                        navigator.clipboard.writeText(matches16[0]).then(() => {
+                            console.log(`16 ${matches16[0]} written to clipboard`);
+                        }).catch(error => {
+                            console.error(error);
+                        });
+                    }, 250);
+                }).catch(error => {
+                    console.error(error);
+                });
+            }, 250);
+        }).catch(error => {
+            console.error(error);
+        });
+    } else {
+        navigator.clipboard.writeText(matches5[0]).then(() => {
+            setTimeout(() => {
+                navigator.clipboard.writeText(matches3[0]).then(() => {
+                    setTimeout(() => {
+                        navigator.clipboard.writeText(matches16[0]).then(() => {
+                        }).catch(error => {
+                            console.error(error);
+                        });
+                    }, 250);
+                }).catch(error => {
+                    console.error(error);
+                });
+            }, 250);
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+})();
+
